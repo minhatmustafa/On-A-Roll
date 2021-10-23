1a9bc4dbdc65d318d1f388f6228ff29d00ab06ae
Minhat Mustafa <mustafa.minnie@gmail.com>
2021-10-23 17:25:10 -0400

    added css and fixed html, js files

--- popup.js
+++ popup.js
@@ -1,10 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var checkPageButton = document.getElementById('My Fridge');
+    var checkPageButton = document.getElementById('My Dashboard');
     checkPageButton.addEventListener('click', function() {
 
         chrome.tabs.getSelected(null, function(tab) {
-            alert("Welcome to your refrigerator!");
+            alert("Welcome to your dashboard!");
         });
     }, false);
 
 }, false);
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     var checkPageButton = document.getElementById('Open Lunchbox');
+//     addEventListener('click', function() {
+//         chrome.tabs.getSelected(null, function(tab) {
+//             chrome.windows.create({
+//                 url: chrome.runtime.getURL("lunchbox.html")
+//             })
+//         })
+//     });
+// }, false);
