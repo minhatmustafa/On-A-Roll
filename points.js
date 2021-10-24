22a8e668bcb051b7aa95df7967c41162d309d4c5
Minhat Mustafa <mustafa.minnie@gmail.com>
2021-10-24 01:10:36 -0400

    added points

---
+++ points.js
@@ -0,0 +1,11 @@
+document.getElementById("check1").addEventListener("click", addPoints);
+document.getElementById("check2").addEventListener("click", addPoints);
+document.getElementById("check3").addEventListener("click", addPoints);
+document.getElementById("check4").addEventListener("click", addPoints);
+
+var points = 0;
+
+function addPoints() {
+    points += 10;
+    document.getElementById('crumbs').innerHTML = points;
+}
